d4bb23d4cbd06663d0893382c9b518019bbcb1f6
Andraina <hei.andraina.4@gmail.com>
2026-04-21 23:58:55 +0300

    feat: adding the CollectivityService with implementation of the methods

---
+++ src/main/java/school/hei/springagricole/controller/CollectivityController.java
@@ -0,0 +1,29 @@
+package school.hei.springagricole.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import school.hei.springagricole.entity.Collectivity;
+import school.hei.springagricole.entity.CreateCollectivity;
+import school.hei.springagricole.exception.BadRequestException;
+import school.hei.springagricole.service.CollectivityService;
+
+import java.sql.SQLException;
+import java.util.List;
+
+@RestController
+@RequestMapping("/collectivities")
+public class CollectivityController {
+    private final CollectivityService collectivityService;
+
+    public CollectivityController(CollectivityService collectivityService) {
+        this.collectivityService = collectivityService;
+    }
+
+    @PostMapping
+
+    public ResponseEntity<List<Collectivity>> createCollectivities(@RequestBody List<CreateCollectivity> requests) {
+        List<Collectivity> collectivities = collectivityService.createCollectivities(requests);
+
+    }
+}
