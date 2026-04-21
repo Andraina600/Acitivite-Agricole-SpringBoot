778bab8725bf6d99047560eb58546fad3dbd9a32
Andraina <hei.andraina.4@gmail.com>
2026-04-21 15:16:32 +0300

    first commit

---
+++ src/test/java/school/hei/springagricole/SpringAgricoleApplicationTests.java
@@ -0,0 +1,13 @@
+package school.hei.springagricole;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAgricoleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
