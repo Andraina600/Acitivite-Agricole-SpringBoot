e52d17d19435201a40502c933167b29e50c7026b
Andraina <hei.andraina.4@gmail.com>
2026-04-21 23:26:49 +0300

    feat: add CreateCollectivity and CreateCollectivityStructure in entity folder

---
+++ src/main/java/school/hei/springagricole/entity/CreateCollectivityStructure.java
@@ -0,0 +1,49 @@
+package school.hei.springagricole.entity;
+
+public class CreateCollectivityStructure {
+    private String president;
+    private String vicePresident;
+    private String treasurer;
+    private String secretary;
+
+    public CreateCollectivityStructure(String president, String vicePresident, String treasurer, String secretary) {
+        this.president = president;
+        this.vicePresident = vicePresident;
+        this.treasurer = treasurer;
+        this.secretary = secretary;
+    }
+
+    public CreateCollectivityStructure() {}
+
+    public String getPresident() {
+        return president;
+    }
+
+    public void setPresident(String president) {
+        this.president = president;
+    }
+
+    public String getVicePresident() {
+        return vicePresident;
+    }
+
+    public void setVicePresident(String vicePresident) {
+        this.vicePresident = vicePresident;
+    }
+
+    public String getTreasurer() {
+        return treasurer;
+    }
+
+    public void setTreasurer(String treasurer) {
+        this.treasurer = treasurer;
+    }
+
+    public String getSecretary() {
+        return secretary;
+    }
+
+    public void setSecretary(String secretary) {
+        this.secretary = secretary;
+    }
+}
