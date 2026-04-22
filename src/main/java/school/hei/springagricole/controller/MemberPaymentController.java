3f99d5a008f2f74041e4a61faafef96ad4e59c8f
Roxann <rofaniry@gmail.com>
2026-04-22 19:46:51 +0300

    feat: add MemberPaymentController

---
+++ src/main/java/school/hei/springagricole/controller/MemberPaymentController.java
@@ -0,0 +1,32 @@
+package school.hei.springagricole.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import school.hei.springagricole.entity.MemberPayment;
+import school.hei.springagricole.service.MemberPaymentService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/members")
+public class MemberPaymentController {
+
+    private final MemberPaymentService memberPaymentService;
+
+    public MemberPaymentController(MemberPaymentService memberPaymentService) {
+        this.memberPaymentService = memberPaymentService;
+    }
+
+    @PostMapping("/{id}/payments")
+    public ResponseEntity<List<MemberPayment>> createPayments(
+            @PathVariable("id") String id,
+            @RequestBody List<MemberPayment> payments) {
+
+        List<MemberPayment> createdPayments = memberPaymentService.createPayments(id, payments);
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(createdPayments);
+    }
+}
