6a2622d8b1f5bfb8bb78c6bacda436078e5fc72a
Andraina <hei.andraina.4@gmail.com>
2026-04-22 16:39:56 +0300

    feat: add PaymentMode enum and MemberPayment in entity folder

---
+++ src/main/java/school/hei/springagricole/entity/MemberPayment.java
@@ -0,0 +1,33 @@
+package school.hei.springagricole.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+public class MemberPayment {
+    private String id;
+    private String memberId;
+    private String membershipFeeId;
+    private String accountCreditedId;
+    private BigDecimal amount;
+    private PaymentMode paymentMode;
+    private LocalDate creationDate;
+
+    private FinancialAccount accountCredited;
+
+    public MemberPayment(String id, String memberId, String membershipFeeId, String accountCreditedId, BigDecimal amount, PaymentMode paymentMode, LocalDate creationDate) {
+        this.id = id;
+        this.memberId = memberId;
+        this.membershipFeeId = membershipFeeId;
+        this.accountCreditedId = accountCreditedId;
+        this.amount = amount;
+        this.paymentMode = paymentMode;
+        this.creationDate = creationDate;
+    }
+
+    public MemberPayment() {}
+}
---
+++ src/main/java/school/hei/springagricole/entity/PaymentMode.java
@@ -0,0 +1,7 @@
+package school.hei.springagricole.entity;
+
+public enum PaymentMode {
+    CASH,
+    MOBILE_BANKING,
+    BANK_TRANSFER
+}
