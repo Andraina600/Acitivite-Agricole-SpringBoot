8ad128a6442e313980f50c7c31917dde97472164
Roxann <rofaniry@gmail.com>
2026-04-22 16:40:13 +0300

    feat: add FinancialAccount, CashAccount, BankAccount, MobileBankingAccount entities and  MobileBankingService, Bank enum

---
+++ src/main/java/school/hei/springagricole/entity/BankAccount.java
@@ -0,0 +1,35 @@
+package school.hei.springagricole.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+public class BankAccount extends FinancialAccount {
+    private String holderName;
+    private Bank bankName;
+    private Integer bankCode;
+    private Integer bankBranchCode;
+    private Long bankAccountNumber;
+    private Integer bankAccountKey;
+
+    public BankAccount() {
+        super();
+        setAccountType("BANK");
+    }
+
+    public BankAccount(String id, String collectivityId, BigDecimal balance,
+                       String holderName, Bank bankName,
+                       Integer bankCode, Integer bankBranchCode,
+                       Long bankAccountNumber, Integer bankAccountKey) {
+        super(id, collectivityId, "BANK", balance);
+        this.holderName = holderName;
+        this.bankName = bankName;
+        this.bankCode = bankCode;
+        this.bankBranchCode = bankBranchCode;
+        this.bankAccountNumber = bankAccountNumber;
+        this.bankAccountKey = bankAccountKey;
+    }
+}
---
+++ src/main/java/school/hei/springagricole/entity/MobileBankingService.java
@@ -0,0 +1,7 @@
+package school.hei.springagricole.entity;
+
+public enum MobileBankingService {
+    ORANGE_MONEY,
+    MVOLA,
+    AIRTEL_MONEY
+}
